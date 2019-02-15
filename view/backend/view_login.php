03ec5d12853ac368c1f7dab7cb93038dadcdd4ac
Đỗ Nhất <dvnit04@gmail.com>
2019-02-15 09:05:54 +0700

    update level user in backend

--- view/backend/view_login.php
+++ view/backend/view_login.php
@@ -1,13 +1,22 @@
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Login</title>
-	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="public/backend/css/style.css">
 </head>
-
+<style type="text/css">
+	 input[type=text] {
+  width: 100%;
+  padding: 15px;
+  margin: 5px 0 22px 0;
+  border: none;
+  background: #f1f1f1;
+}
+</style>
 <body>
 	<div class="login">
 		<div class="form_login">
